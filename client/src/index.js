93fe0bcb9cbec9f774986c3c6a57801950724c12
Rohit Raj <irohit427@gmail.com>
2020-09-24 22:11:54 +0530

    Implemented client

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import NewMovies from './components/NewMovies';
+import MovieInfo from './components/MovieInfo';
+import './style/style.scss';
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+      <Switch >
+        <Route exact path="/" component={NewMovies} />
+        <Route exact path="/info/:id" component={MovieInfo} />
+      </Switch>
+      </ApolloProvider>
+    </BrowserRouter>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Root />,
+  document.querySelector('#root')
 );
--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema.js')
+const cors = require('cors')
 
 dotenv.config();
 const app = express();
 //app.use(webpackMiddleware(webpack(webpackConfig)));
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello');
 })
